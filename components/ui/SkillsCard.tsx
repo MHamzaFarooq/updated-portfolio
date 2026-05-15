bd829be012b8cfe214aed6a27f7761fe7227b176
Hamza <hamzafarooq49ml@gmail.com>
2026-05-15 07:40:06 +0500

    inserted buttons

--- components/ui/SkillsCard.tsx
+++ components/ui/SkillsCard.tsx
@@ -17,6 +17,7 @@ function SkillsCard({
     <motion.div
       initial={{ opacity: 0, y: 100 }}
       whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
       transition={{
         duration: 1.75,
         delay: Number(id) * 0.1,
